bc10fbb096dbf0cbec1e3d09a2e833794f18b841
jerife <jerife@naver.com>
2022-03-28 15:54:05 +0900

    feat: before file deleted

--- static/javascript/navbar.js
+++
@@ -1,5 +0,0 @@
-let toggleBtn = document.querySelector(".navbar__toggleBtn");
-let menu = document.querySelector(".navbar__menu");
-toggleBtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
